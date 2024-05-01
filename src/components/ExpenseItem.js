895c901d0689061483dedf9a7dfb037d9a487609
dudtjq <dudtjq0415@naver.com>
2024-05-01 17:18:29 +0900

    컴포넌트 사용 해보기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -19,7 +19,7 @@ const ExpenseItem = ({title, price, date}) => {
   }
 
   // 숫자를 화폐 표기법으로 바꾸기
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(price);
+  const formattedPirce = new Intl.NumberFormat('ko-KR',{ style: 'currency', currency: 'KRW' }).format(price)
 
 
   return (
@@ -27,7 +27,7 @@ const ExpenseItem = ({title, price, date}) => {
     <div>{makeFormattedDate()}</div>
     <div className='expense-item__description'>
       <h2>{title}</h2>
-      <div className='expense-item__price'>{formattedPrice}원</div>
+      <div className='expense-item__price'>{formattedPirce}원</div>
     </div>
   </div>
   )
