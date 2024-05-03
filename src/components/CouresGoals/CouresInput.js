2e559ae6063b279bba1e620cc00f3cb717247b49
dudtjq <dudtjq0415@naver.com>
2024-05-03 14:46:58 +0900

    입력값 유효성 동적 검사

--- src/components/CouresGoals/CouresInput.js
+++ src/components/CouresGoals/CouresInput.js
@@ -4,13 +4,23 @@ import Button from '../UI/Button/Button';
 
 const CouresInput = ({ onAdd }) => {
   const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증 상태 변수
+  const [isVaild, setIsVaild] = useState(true);
 
   const textChangeHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsVaild(isVaild);
+    }
     setEnteredText(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (enteredText.trim().length === 0) {
+      setIsVaild(false);
+      return;
+    }
 
     onAdd(enteredText);
     setEnteredText('');
@@ -20,7 +30,15 @@ const CouresInput = ({ onAdd }) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" onChange={textChangeHandler} value={enteredText} />
+        <input
+          style={{
+            background: isVaild ? 'transparent' : 'salmon',
+            borderColor: isVaild ? 'black' : 'red',
+          }}
+          type="text"
+          onChange={textChangeHandler}
+          value={enteredText}
+        />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
