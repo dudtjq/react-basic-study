7b4149754d47605dc4b2b05c572d42d7b8fbe1c0
dudtjq <dudtjq0415@naver.com>
2024-05-03 16:50:33 +0900

    유저 인풋 적용 및 모달 에러 추가 커밋

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
 import './App.css';
 import React, { useState } from 'react';
-import CouresInput from './components/CouresGoals/CouresInput';
-import CouresList from './components/CouresGoals/CouresList';
-
-const DUMMY_DATA = [
-  {
-    id: 'g1',
-    text: '리엑트 컴포넌트 마스터 하기',
-  },
-  {
-    id: 'g2',
-    text: 'UI 프로그래밍 삽고수 되기',
-  },
-];
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
 
 const App = () => {
-  const [goals, setGoals] = useState(DUMMY_DATA);
-
-  // Input 에게 전달할 함수
-  const addGoalHandler = (text) => {
-    console.log('전달 받은 텍스트 : ', text);
-    const newGoal = {
-      id: Math.random().toString(),
-      text,
-    };
-
-    // 상태변수(배열) 수정
-    //setGoals([...goals, newGoal]);
-    setGoals((prevGoals) => [...prevGoals, newGoal]);
-  };
-
-  // 삭제 이벤트 핸들러를 CouresItem까지 내려 보내야함
-  const deleteGoalHandler = (id) => {
-    // console.log('전달된 id :', id);
-    // const updateGoals = [...goals]; // 상태 배열 그대로 복사해서 가져옴
-    // const index = updateGoals.findIndex((goal) => goal.id === id);
-    // updateGoals.splice(index, 1);
-    // setGoals(updateGoals);
-
-    // const updateGoals = goals.filter((goal) => goal.id !== id);
-    setGoals(goals.filter((goal) => goal.id !== id));
-  };
-
-  // CouresList 조건부 렌더링
-  let listContent = (
-    <p
-      style={{
-        color: 'red',
-        fontSize: '2em',
-        textAlign: 'center',
-      }}
-    >
-      목표를 등록해 주세요.
-    </p>
-  );
-
-  if (goals.length > 0) {
-    listContent = <CouresList items={goals} onDelete={deleteGoalHandler} />;
-  }
-
   return (
     <div>
-      <section id="goal-form">
-        <CouresInput onAdd={addGoalHandler} />
-        <section id="goals">{listContent}</section>
-      </section>
+      <AddUsers />
+      <UserList />
     </div>
   );
 };
